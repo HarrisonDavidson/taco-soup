ef2aa5720d246a27ea181d45f5de9d3d75acd516
Harrison Davidson <hossdavidson@gmail.com>
2023-02-15 15:13:45 -0500

    Taco-Soup

---
+++ main.js
@@ -0,0 +1,65 @@
+// Exercise 1
+// var harryPotterTitles = ["and the Sorcerer's Stone", "and the Chamber of Secrets", "and the Prisoner of Azkaban", "and the Goblet of Fire", "and the Order of the Phoenix", "and the Half-Blood Prince", "and the Deathly Hallows"];
+// for(var i = 0; i < harryPotterTitles.length; i++){
+//     console.log(harryPotterTitles[i]);
+// }
+
+//Exercise 2
+// var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92];
+// let numberOfAs = 0;
+// let numberOfBs = 0;
+// let numberOfCs = 0;
+// let numberOfDs = 0;
+// let numberOfFs = 0;
+// let mostCommonGradeCount = 0;
+// let mostCommonGrade = 0;
+// let averagePercentageGrade = 0;
+// let totalGradePoints = (grades.length);
+
+// for(i = 0; i < grades.length; i++){
+//     if (grades[i] >= 0 && grades[i] <=69){
+//         numberOfFs++
+//     } else if (grades[i] >= 70 && grades[i] <= 76){
+//         numberOfDs++
+//     } else if (grades[i] >= 77 && grades[i] <= 84){
+//         numberOfCs++
+//     } else if (grades[i] >=85 && grades[i]<= 92){
+//         numberOfBs++
+//     } else if (grades[i] >= 93 && grades[i]<= 100){
+//         numberOfAs++
+//     }
+// }
+
+// for (i = 0; i < grades.length; i++)
+// {
+//     if (mostCommonGradeCount < numberOfFs){
+//         mostCommonGradeCount = numberOfFs
+//         mostCommonGrade = "F"
+// } else if (mostCommonGradeCount < numberOfDs){
+//     mostCommonGradeCount = numberOfDs
+//     mostCommonGrade = "D"
+// } else if (mostCommonGradeCount < numberOfCs){
+//     mostCommonGradeCount = numberOfCs
+//     mostCommonGrade = "C"
+// } else if (mostCommonGradeCount < numberOfBs){
+//     mostCommonGradeCount = numberOfBs
+//     mostCommonGrade = "B"
+// } else if (mostCommonGradeCount < numberOfAs){
+//     mostCommonGradeCount = numberOfAs
+//     mostCommonGrade = "A"
+// }
+// }
+
+// for (i = 0; i < grades.length; i++){
+//     totalGradePoints += grades[i]
+//     }
+//     averagePercentageGrade = totalGradePoints / grades.length
+
+// console.log(`There are ${numberOfAs} As in this class`)
+// console.log(`There are ${numberOfBs} Bs in this class`)
+// console.log(`The most common letter grade is ${mostCommonGrade}`)
+// console.log(`The total is ${totalGradePoints}`)
+// console.log(`The average percentage grade is ${Math.ceil(averagePercentageGrade)}%`)
+// console.log(totalGradePoints)
+
+// Exercise 3
