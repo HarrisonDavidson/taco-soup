13d4416598784a57d4200d21de1d93605baabec0
Harrison Davidson <hossdavidson@gmail.com>
2023-02-16 10:25:23 -0500

    taco-soup

--- main.js
+++ main.js
@@ -86,3 +86,37 @@
 //     } sentence += ` ${sentenceArray[a]}`
 //     }
 //     console.log(sentence)
+
+// Exercise 4 - The Beatles
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+console.log(`${beatles.members[1].name} was in the Beatles from ${beatles.history.formed} to ${beatles.history.disbanded}. He was born in ${beatles.members[1].birth}. He contributed heavily to the ${beatles.albums[0]} Album.`);
